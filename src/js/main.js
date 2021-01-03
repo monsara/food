477fdd839b560769276cd8ead3ce2739a9ddfa7b
Virtor Hirenko <viktor.hirenko@gmail.com>
2021-01-03 23:58:50 +0200

    add logic for hide showing and hiding tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,54 @@
+'use strict';
 
+window.addEventListener('DOMContentLoaded', () => {
+    const refs = {
+        tabs: document.querySelectorAll('.tabheader__item'),
+        tabsContent: document.querySelectorAll('.tabcontent'),
+        tabsParent: document.querySelector('.tabheader__items'),
+    };
+
+    refs.tabsParent.addEventListener('click', handleTabsParentClick);
+
+    hideTabContent();
+    showTabContent();
+
+    /* ---------------------------- Functions ------------------------------- */
+    function handleTabsParentClick(event) {
+        const target = event.target;
+
+        if (target && target.matches('.tabheader__item')) {
+            refs.tabs.forEach((tab, i) => {
+                if (target === tab) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+
+        setActiveLink(target);
+    }
+
+    function hideTabContent() {
+        refs.tabsContent.forEach(tabContent => {
+            tabContent.classList.add('hide');
+            tabContent.classList.remove('show', 'fade');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        refs.tabsContent[i].classList.add('show', 'fade');
+        refs.tabsContent[i].classList.remove('hide');
+    }
+
+    function setActiveLink(currentLink) {
+        const activeLink = currentLink.parentNode.querySelector(
+            '.tabheader__item_active');
+
+        if (activeLink) {
+            activeLink.classList.remove(
+                'tabheader__item_active');
+        }
+
+        currentLink.classList.add('tabheader__item_active');
+    }
+});
