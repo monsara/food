c741a3074c7ba7771b8fa12cd71fd63e6594eb2a
Virtor Hirenko <viktor.hirenko@gmail.com>
2021-01-12 21:37:33 +0200

    install axios

--- src/js/main.js
+++ src/js/main.js
@@ -232,8 +232,23 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     }
 
-    getResource('http://localhost:3000/menu').then(data => {
-        data.forEach(({ img, altimg, title, descr, price }) => {
+    // v.1: with fetch API
+    // getResource('http://localhost:3000/menu').then(data => {
+    //     data.forEach(({ img, altimg, title, descr, price }) => {
+    //         new MenuCard(
+    //             img,
+    //             altimg,
+    //             title,
+    //             descr,
+    //             price,
+    //             '.menu .container',
+    //         ).render();
+    //     });
+    // });
+
+    // v.2: with fetch Axios
+    axios.get('http://localhost:3000/menu').then(data => {
+        data.data.forEach(({ img, altimg, title, descr, price }) => {
             new MenuCard(
                 img,
                 altimg,
